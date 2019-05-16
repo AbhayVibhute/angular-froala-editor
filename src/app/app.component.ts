5e2ab6c20b5e3cd60e7c776f2d14cea701be1c11
AbhayVibhute <abhayvibhute94@gmail.com>
2019-05-16 12:47:11 +0530

    froala editor integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControlName, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'froala-editor';
+  froalaEditor: FormGroup;
+
+  // tslint:disable-next-line:ban-types
+  public options: Object = {
+    // placeholderText: 'Edit Your Content Here!',
+    charCounterCount: false,
+    toolbarButtons: ['bold', 'italic', 'underline', 'paragraphFormat'],
+    // toolbarButtonsXS: ['bold', 'italic', 'underline', 'paragraphFormat', 'alert'],
+    // toolbarButtonsSM: ['bold', 'italic', 'underline', 'paragraphFormat', 'alert'],
+    // toolbarButtonsMD: ['bold', 'italic', 'underline', 'paragraphFormat', 'alert'],
+    // events: {
+    //   'froalaEditor.focus'(e, editor) {
+    //     console.log(editor.selection.get());
+    //   }
+    // }
+  };
+
+  public editorContent = 'My Document\'s Title';
+
+  ngOnInit() {
+    this.froalaEditor = new FormGroup({
+      editor: new FormControl(null)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.froalaEditor.value);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
